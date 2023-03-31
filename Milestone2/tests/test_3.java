befad6914cabe6a8f4c94bf245d14fa8d9132d90
Shrey Mehta <79155940+Shrey0601@users.noreply.github.com>
2023-03-31 23:46:05 +0530

    Add files via upload

---
+++ Milestone2/tests/test_3.java
@@ -0,0 +1,18 @@
+public class SyntaxCheck {
+    static void printFunc(float val) {
+        //System.out.println(val);
+    }
+
+    public static void main(String[] args) {
+        int a;
+        a = 5;
+        a++;
+        printFunc(a);
+        ++a;
+        float b = 3.14f; // Default to a float type
+        printFunc(b);
+
+        long val = 100L;
+        printFunc(val);
+    }
+}
